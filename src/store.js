ec34c0d1f4da09e01c3eedabae6eb69637e5a409
henry5059 <henryliriano5059@gmail.com>
2020-11-26 16:30:36 -0400

    Correccion direccion

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 
 // const _host = "http://192.168.1.104:93";
 // const _host = "http://172.20.1.12:303";
-const _host = "https://odyssey-api.cmsiglo21.app/api";
+const _host = "https://odyssey-api.cmsiglo21.app";
 
 const axiosInstance = Axios.create({
   baseURL: _host + "/api"
